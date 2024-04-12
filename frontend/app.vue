63a47274b85415e938d653e4234cf4655814de98
Me <getdaniel>
2024-04-13 01:46:43 +0300

    install apollo, vee-validate and tailwindcss

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <h1> Hello Nuxt</h1>
+  <div class="bg-green700 h-screen items-center">
+    <h2> I am Daniel, I will tackle you soon! </h2>
   </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@nuxtjs/apollo',
+    '@vee-validate/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://spacex-production.up.railway.app'
+      }
+    },
+  },
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray',
+      ErrorMessage: 'VeeErrorMessage',
+    },
+  },
+  tailwindcss: {}
 })
