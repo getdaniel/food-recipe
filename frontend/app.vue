ffd060e6b39658e72641d051c5c4ee8e62860cce
Me <getdaniel>
2024-04-13 20:15:42 +0300

    adjust the header

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,9 +1,11 @@
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <div class="text-3xl font-bold underline flex items-center justify-center h-screen">
-    <h2> I am Daniel, I will tackle you soon! </h2>
+  <div class="bg-gray-600">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <div class="text-3xl font-bold underline flex items-center justify-center h-screen">
+      <h2> I am Daniel, I will tackle you soon! </h2>
+    </div>
   </div>
 </template>
--- frontend/components/TheHeader.vue
+++ frontend/components/TheHeader.vue
@@ -1,32 +1,64 @@
 <template>
-  <header class="bg-green-600 text-white p-4">
-    <div class="container mx-auto flex items-center justify-between">
+  <div class="bg-sky-950 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
 
-      <!-- Logo and Title -->
-      <div class="flex items-center">
+    <!-- Logo and Title -->
+    <div class="flex items-center">
+      <span class="text-green-500 text-xl mr-1">
         <div class="rounded-full overflow-hidden mr-4">
-          <img src="../statics/logo.png" alt="Logo" class="h-12 w-12">
+          <img src="../statics/logo.png" alt="Logo" class="h-10 w-12">
         </div>
-      </div>
-
-      <!-- Navigation Menu -->
-      <nav class="md:flex space-x-4 mt-4 md:mt-0">
-        <ul class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <!-- Main Navigation Links -->
-          <li><a href="#" class="hover:text-gray-300">Home</a></li>
-          <li><a href="#" class="hover:text-gray-300">Browse Recipes</a></li>
-          <li><a href="#" class="hover:text-gray-300">Categories</a></li>
-          <li><a href="#" class="hover:text-gray-300">Sign Up</a></li>
-          <li><a href="#" class="hover:text-gray-300">Log In</a></li>
-        </ul>
-      </nav>
-
-      <!-- Navigation Toggle Button -->
-      <button class="block md:hidden focus:outline-none">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-        </svg>
-      </button>
+      </span>
+      <h1 class="text-xl"> Recipes </h1>
     </div>
-  </header>
+
+    <!-- Navigation Toggle Button -->
+    <button class="absolute md:hidden right-6 top-4 cursor-pointer" @click="open = !open">
+      <svg v-if="open" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+      </svg>
+      <svg v-else class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+      </svg>
+    </button>
+
+    <!-- Navigation Menu -->
+    <nav>
+      <ul
+        class="md:flex md:items-center md:px-0 px-3 md:pb-0 pb-10 md:static absolute bg-sky-950 md:w-auto w-full top-14 duration-700 ease-in"
+        :class="[open ? 'left-0' : 'left-[-100%]']">
+        <!-- Main Navigation Links -->
+        <li class="md:mx-4 md:my-0 my-6" v-for="link in Links">
+          <a href={{ link.link }} class="text-xl hover:text-green-500">{{ link.name }}</a>
+        </li>
+        <BaseButton> Get Started </BaseButton>
+      </ul>
+    </nav>
+
+  </div>
 </template>
+
+<script>
+
+import BaseButton from './BaseButton';
+
+export default {
+
+  components: {
+    BaseButton
+  },
+
+  setup() {
+    let open = ref(false);
+    let Links = [
+      { name: "Home", link: "#" },
+      { name: "Recipes", link: "#" },
+      { name: "Categories", link: "#" },
+    ]
+
+    return { Links, open };
+  }
+}
+
+</script>
