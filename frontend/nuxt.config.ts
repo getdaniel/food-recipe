ab7096a1e4678719b3e97bc673691deaae154a52
Me <getdaniel>
2024-04-14 11:41:10 +0300

    entire config of the apollo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   apollo: {
     clients: {
       default: {
-        httpEndpoint: 'https://spacex-production.up.railway.app'
+        httpEndpoint: 'http://localhost:4000',
       }
     },
   },
