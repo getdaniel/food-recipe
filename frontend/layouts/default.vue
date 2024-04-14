92403e7023a11bdfd49fec703c077d2f7d5a45db
Me <getdaniel>
2024-04-14 23:57:05 +0300

    home page with recipe card

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,18 @@
 <!-- The default layout (header, footer included here) -->
 <template>
   <div>
-    <TheHeader /> 
-    <TheSearch />
-    <RecipeCard />
+    <TheHeader />
+    <div class="min-h-screen">
+      <TheSearch />
+      <RecipeCard />
+    </div>
     <TheFooter />
   </div>
 </template>
-  
+
 <script setup>
-  import TheHeader from '~/components/TheHeader.vue'
-  import TheSearch from '~/components/SearchBar.vue'
-  import TheFooter from '~/components/TheFooter.vue'
-  import RecipeCard from '~/components/RecipeCard.vue'
+import TheHeader from '~/components/TheHeader.vue'
+import TheSearch from '~/components/SearchBar.vue'
+import TheFooter from '~/components/TheFooter.vue'
+import RecipeCard from '~/components/RecipeCard.vue'
 </script>
