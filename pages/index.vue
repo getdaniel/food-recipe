9b27057b7dbd5cde136477e5a4d508a51df04285
Me <getdaniel>
2024-04-21 09:39:10 +0300

    try to remove apollo func

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,5 @@
 <template>
     <div class="text-center min-h-screen">
         Hello... I am comming soon!
-        <p>{{ data }}</p>
     </div>
 </template>
-
-<script lang="ts" setup> 
-const query = gql`
-query {
-  users {
-    id
-    username
-    email
-  }
-}`
-
-const { data } = await useAsyncQuery(query)
-
-</script>
