d3d4c491746df4d4cc8174d42504c5662edd1dc2
Me <getdaniel>
2024-04-23 19:35:59 +0300

    finishing the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,15 @@
 <template>
-  <div class="text-center min-h-screen mt-10">
-    <p v-if="data?.users?.length === 0">No users available.</p>
-    <div v-else>
-      <p v-for="user in data?.users" :key="user.id">
-        Username: {{ user.username }}, <br /> Email: {{ user.email }}
-      </p>
-    </div>
+  <div class="min-h-screen">
+    <Slideshow />
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+import Slideshow from '~/components/Slideshow.vue';
 
-interface User {
-  id: string;
-  username: string;
-  email: string;
-}
-
-interface UserData {
-  users: User[];
-}
-
-const query = gql`
-  query MyQuery {
-    users {
-      id
-      username
-      email
-    }
+export default {
+  components: {
+    Slideshow
   }
-`;
-
-const { data } = await useAsyncQuery<UserData>(query);
+};
 </script>
